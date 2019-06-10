0763afa6642d3967542cad4542cd9e8112495f7c
TashDubrovska <n.dubrovska89@gmail.com>
2019-06-10 03:40:18 +0100

    Fix gitignore and improve env variable usage

--- send_message.py
+++ send_message.py
@@ -1,11 +1,11 @@
 import requests
-from dotenv import dotenv_values
-
-env_variables = dotenv_values()
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 def send_message(email, message):
-  mail_api_key= env_variables["MAILGUN_API_KEY"]
-  mail_domain = env_variables["MAILGUN_API_DOMAIN"]
+  mail_api_key= os.getenv("MAILGUN_API_KEY")
+  mail_domain = os.getenv("MAILGUN_API_DOMAIN")
 
   return requests.post(
     "https://api.mailgun.net/v3/{}/messages".format(mail_domain),
